2ef158018e317e63e47a4ca57d24307e533290fa
TheArtemis <lordef01@gmail.com>
2023-09-26 18:00:27 +0200

    Fixed bug in image conversion

--- scripts/build_test_gt.py
+++ scripts/build_test_gt.py
@@ -25,21 +25,29 @@ for name in img_names:
     print(name)
     img = np.array(Image.open('data/38-Cloud_test/Entire_scene_gts/edited_corrected_gts_' + name + '.TIF'))
     img_x, img_y = img.shape
+
+    #print(img)
     
+    margin_x = 0
+    margin_y = 0
+
     if img_x % 384 != 0:
         margin_x = 384 - img_x % 384
     if img_y % 384 != 0:
         margin_y = 384 - img_y % 384    
 
     new_img = np.zeros((img_x + margin_x, img_y + margin_y))
-    new_img[margin_x//2:img_x + margin_x//2, margin_y//2:img_y + margin_y//2] = img    
-    s = Image.fromarray(new_img) # TEMPORARY    
+    new_img = np.pad(img, ((margin_x//2, margin_x - margin_x//2), (margin_y//2, margin_y - margin_y//2)), mode='constant')
+    new_img = np.where(new_img == True, 255, 0).astype(np.uint8)     
+    s = Image.fromarray(new_img) # TEMPORARY
+    s.save('test/' + name + '.TIF') # TEMPORARY
 
     x = 1
     for i in range(0, new_img.shape[0], 384):
         for j in range(0, new_img.shape[1], 384):
-            patch = new_img[i:i+384, j:j+384]
-            patch = Image.fromarray(patch)
+            patch = new_img[i:i+384, j:j+384]                  
+            #print(patch)
+            patch = Image.fromarray(patch)            
             patch_name = f'data/38-Cloud_test/test_gt/gt_patch_{x}_{(i//384+1)}_by_{(j//384+1)}_{name}.TIF'            
             #print('saving: ', f'data/38-Cloud_test/test-gt/gt_patch_{i//384+j//384}_{i//384}_{j//384}_{name}.TIF')
             patch.save(patch_name)
