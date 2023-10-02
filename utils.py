c12f748e362b01994a08fc3de089a0aaa973df5a
TheArtemis <lordef01@gmail.com>
2023-10-02 18:51:47 +0200

    Added dice score

--- utils.py
+++ utils.py
@@ -13,6 +13,7 @@ def load_checkpoint(checkpoint, model):
 def check_accuracy(loader, model, device='cuda'):
     num_correct = 0
     num_pixels = 0
+    dice_score = 0
     model.eval()
 
     with torch.no_grad():
@@ -25,25 +26,26 @@ def check_accuracy(loader, model, device='cuda'):
 
             num_correct += (preds == y).sum()
             num_pixels += torch.numel(preds)
+            dice_score += (2*(preds*y).sum()) / ((preds+y).sum()+1e-8)
 
-    print(
-        f'Got {num_correct}/{num_pixels} with acc {num_correct/num_pixels*100:.2f}'
-    )
+    print(f'Got {num_correct}/{num_pixels} with acc {num_correct/num_pixels*100:.2f}')
+    print(f'Dice score: {dice_score/len(loader)}')
+    model.train()
 
 def save_predictions_as_imgs(loader, model, folder='imgs/', device='cuda'):
-    model.eval()
+    model.eval()    
     for idx, (x, y) in enumerate(loader):
-        x = x.to(device=device)
-        y = y.to(device=device)
+
+        if idx > 30: # Save only the first 20 predictions
+            break
+
+        x = x.to(device=device)        
         with torch.no_grad():
             preds = torch.sigmoid(model(x))
             preds = (preds > 0.5).float()
 
         torchvision.utils.save_image(
             preds, f'{folder}/pred_{idx}.png'
-        )       
-
-        torchvision.utils.save_image(
-            y.float().unsqueeze(1), f'{folder}/mask_{idx}.png'
         )
-        #print(y.shape)
+
+    model.train()      
